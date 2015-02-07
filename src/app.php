5857b3bde24a7429ff82cb2a81ff0b4e65c60b71
Robert <kel.robert@gmail.com>
2015-02-07 13:14:58 +0000

    Different layers

--- src/app.php
+++ src/app.php
@@ -4,21 +4,9 @@ use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'World',
-    '_controller' => function ($request) {
-        // $foo will be available in the template
-        $request->attributes->set('foo', 'bar');
-
-        $response = render_template($request);
-
-        // change some header
-        $response->headers->set('Content-Type', 'text/html');
-
-        return $response;
-    }
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
-
-$routes->add('bye', new Routing\Route('/bye'));
 
 return $routes;
