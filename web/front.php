bb911602165aad2f8a6bb64a3ae533a31670e5ac
Robert <kel.robert@gmail.com>
2015-02-07 15:46:08 +0000

    Tests

--- web/front.php
+++ web/front.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
 
